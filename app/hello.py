85b2ecbefe2ccd30b23a14779212c376d9d9448d
bittles <bauern.sc@gmail.com>
2023-01-25 18:02:08 -0500

    wait prior to trying first snapshot

--- app/hello.py
+++ app/hello.py
@@ -264,6 +264,7 @@ class Connector:
 
 
 async def SnapshotInterval(CAM_NAME, PORTNUM, INTERVAL):
+    await asyncio.sleep(120)
     while True:
         try:
             os.system('ffmpeg -y -i rtsp://127.0.0.1:' + str(PORTNUM) + '/' + CAM_NAME + '-frames:v 1  /config/www/eufyp2p/' + CAM_NAME + '.jpg')
